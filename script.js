71ce27be06e35b2182d6cfa3594f4713a497918f
nikithanikithakavali-collab <nikithanikithakavali@gmail.com>
2026-07-19 21:57:21 +0530

    Add weather API call functionality

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ button.addEventListener("click", function () {
     document.getElementById("result").innerHTML = "You searched for: " + city;
 
 });
+const apiKey = "fed08b4198e3073d991a959d06a551a7";
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", function () {
+
+    const city = document.getElementById("city").value;
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    console.log(url);
+
+});
